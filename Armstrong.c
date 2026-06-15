db18bbffe3861fe2e1677e1dcdf04a7019cbd2ad
Rishabh Kaushik <111851115+Rishabhqwertyuiop@users.noreply.github.com>
2026-06-15 15:15:36 +0530

    Create Armstrong.c

---
+++ Armstrong.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int num, originalNum, remainder;
+    int result = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    originalNum = num;
+
+    while (num != 0) {
+        remainder = num % 10;
+        result += remainder * remainder * remainder;
+        num /= 10;
+    }
+
+    if (result == originalNum)
+        printf("%d is an Armstrong Number\n", originalNum);
+    else
+        printf("%d is Not an Armstrong Number\n", originalNum);
+
+    return 0;
+}
