f45f3358d5d4117a1ecc950ed5c214c73db5f438
Rishabh Kaushik <111851115+Rishabhqwertyuiop@users.noreply.github.com>
2026-06-15 15:14:03 +0530

    Create Palindrome.c

---
+++ Palindrome.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int num, originalNum, reverse = 0, remainder;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    originalNum = num;
+
+    while (num != 0) {
+        remainder = num % 10;
+        reverse = reverse * 10 + remainder;
+        num = num / 10;
+    }
+
+    if (originalNum == reverse)
+        printf("%d is a Palindrome Number\n", originalNum);
+    else
+        printf("%d is Not a Palindrome Number\n", originalNum);
+
+    return 0;
+}
