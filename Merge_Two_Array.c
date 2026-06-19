b1d6fdfb1b1a785d0b35450c11b4cfec1f452095
Rishabh Kaushik <rishabh.kaushik@orange.com>
2026-06-19 12:43:19 +0530

    Updated

---
+++ Merge_Two_Array.c
@@ -0,0 +1,35 @@
+int main() {
+    int arr1[5], arr2[5], arr3[10];
+    int i, j = 0;
+
+    printf("Enter 5 elements of first array:\n");
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &arr1[i]);
+    }
+
+    printf("Enter 5 elements of second array:\n");
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &arr2[i]);
+    }
+
+    // Copy first array to arr3
+    for (i = 0; i < 5; i++) {
+        arr3[j] = arr1[i];
+        j++;
+    }
+
+    // Copy second array to arr3
+    for (i = 0; i < 5; i++) {
+        arr3[j] = arr2[i];
+        j++;
+    }
+
+    printf("Merged array:\n");
+
+    for (i = 0; i < 10; i++) {
+        printf("%d ", arr3[i]);
+    }
+
+    return 0;
+}
+
